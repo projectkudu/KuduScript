bec1170f47c61a6529e6b41d0b6a98832cfc9b59
Ahmed ElSayed <ahmels@microsoft.com>
2016-05-16 17:37:37 -0700

    Support ASP.NET Core RC2

--- bin/kuduscript.js
+++ bin/kuduscript.js
@@ -9,7 +9,7 @@ function addDeploymentScriptOptions(command) {
        .description('Generate custom deployment script')
        .option('-r, --repositoryRoot [dir path]', 'The root path for the repository (default: .)')
        .option('--aspWAP <projectFilePath>', 'Create a deployment script for .NET web application, specify the project file path')
-       .option('--aspNet5 <projectFilePath>', 'Create a deployment script for ASP.NET 5 web application, specify the project.json file path')
+       .option('--aspNetCore <projectFilePath>', 'Create a deployment script for ASP.NET Core web application, specify the project.json file path')
        .option('--aspWebSite', 'Create a deployment script for basic website')
        .option('--go', 'Create a deployment script for Go website')
        .option('--node', 'Create a deployment script for node.js website')
@@ -23,28 +23,20 @@ function addDeploymentScriptOptions(command) {
        .option('-o, --outputPath <output path>', 'The path to output generated script (default: same as repository root)')
        .option('-y, --suppressPrompt', 'Suppresses prompting to confirm you want to overwrite an existing destination file.')
        .option('--no-dot-deployment', 'Do not generate the .deployment file.')
-       .option('--no-solution', 'Do not require a solution file path (only for --aspWAP otherwise ignored).')
-       .option('--aspNet5Version <version>', 'The Dnx version for ASP.NET 5')
-       .option('--aspNet5Runtime <runtime>', 'The .NET runtime (clr vs coreclr) for ASP.NET 5')
-       .option('--aspNet5Architecture <architecture>', 'The architecture (x64 vs x86) for ASP.NET 5')
-       .option('--dnxConsoleApp <projectDirectory>', 'Creates a deployment script for a DNX base console app as a webjob');
+       .option('--no-solution', 'Do not require a solution file path (only for --aspWAP otherwise ignored).');
 }
 
 function deploymentScriptExecute(name, options, log, confirm, _) {
   var repositoryRoot = options.repositoryRoot || '.';
   var outputPath = options.outputPath || repositoryRoot;
   var scriptType = options.scriptType;
-  var projectFile = options.aspWAP || options.dotNetConsole || options.aspNet5;
+  var projectFile = options.aspWAP || options.dotNetConsole || options.aspNetCore;
   var solutionFile = options.solutionFile;
   var sitePath = options.sitePath || repositoryRoot;
   var noDotDeployment = options.dotDeployment === false;
   var noSolution = options.solution === false;
-  var aspNet5Version = options.aspNet5Version;
-  var aspNet5Runtime = options.aspNet5Runtime;
-  var aspNet5Architecture = options.aspNet5Architecture;
-  var dnxConsoleApp = options.dnxConsoleApp;
 
-  var exclusionFlags = [options.aspWAP, options.php, options.python, options.aspWebSite, options.node, options.basic, options.dotNetConsole, options.aspNet5, options.go, options.dnxConsoleApp];
+  var exclusionFlags = [options.aspWAP, options.php, options.python, options.aspWebSite, options.node, options.basic, options.dotNetConsole, options.aspNetCore, options.go];
   var flagCount = 0;
   for (var i in exclusionFlags) {
     if (exclusionFlags[i]) {
@@ -55,17 +47,17 @@ function deploymentScriptExecute(name, options, log, confirm, _) {
   if (flagCount === 0) {
     options.helpInformation();
     log.help('');
-    log.help('Please specify one of these flags: --aspWAP, --aspWebSite, --php, --python, --dotNetConsole, --basic or --node');
+    log.help('Please specify one of these flags: --aspWAP, --aspNetCore, --aspWebSite, --php, --python, --dotNetConsole, --basic or --node');
     return;
   } else if (flagCount > 1) {
-    throw new Error('Please specify only one of these flags: --aspWAP, --aspWebSite, --php, --python, --dotNetConsole, --basic or --node');
+    throw new Error('Please specify only one of these flags: --aspWAP, --aspNetCore, --aspWebSite, --php, --python, --dotNetConsole, --basic or --node');
   }
 
   var projectType;
   if (options.aspWAP) {
     projectType = generator.ProjectType.wap;
-  } else if (options.aspNet5) {
-    projectType = generator.ProjectType.aspNet5
+  } else if (options.aspNetCore) {
+    projectType = generator.ProjectType.aspNetCore
   } else if (options.aspWebSite) {
     projectType = generator.ProjectType.website;
   }else if (options.go) {
@@ -76,8 +68,6 @@ function deploymentScriptExecute(name, options, log, confirm, _) {
       projectType = generator.ProjectType.python;
   } else if (options.dotNetConsole) {
     projectType = generator.ProjectType.dotNetConsole;
-  } else if (options.dnxConsoleApp) {
-    projectType = generator.ProjectType.dnxConsoleApp;
   } else {
     projectType = generator.ProjectType.basic;
   }
@@ -87,7 +77,7 @@ function deploymentScriptExecute(name, options, log, confirm, _) {
     confirmFunc = function (message, callback) { callback(undefined, true); };
   }
 
-  var scriptGenerator = new generator.ScriptGenerator(repositoryRoot, projectType, projectFile, solutionFile, sitePath, scriptType, outputPath, noDotDeployment, noSolution, aspNet5Version, aspNet5Runtime, aspNet5Architecture, dnxConsoleApp, log, confirmFunc);
+  var scriptGenerator = new generator.ScriptGenerator(repositoryRoot, projectType, projectFile, solutionFile, sitePath, scriptType, outputPath, noDotDeployment, noSolution, log, confirmFunc);
   scriptGenerator.generateDeploymentScript(_);
 }
 
